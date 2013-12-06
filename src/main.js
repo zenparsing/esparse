96558cc2317214ec14c35b3bbcca2ff6f9eadb2e
khs4473 <zenparsing@gmail.com>
2013-12-06 10:08:26 -0500

    Remove exported forEachNode (now a method of AST.Node).

--- src/main.js
+++ src/main.js
@@ -14,35 +14,3 @@ export function parseScript(input, options) {
 
     return new Parser(input, options).Script();
 }
-
-export function forEachChild(node, fn) {
-
-    var keys = Object.keys(node), val, i, j;
-    
-    for (i = 0; i < keys.length; ++i) {
-    
-        // Don't iterate over backlink to parent
-        if (keys[i] === "parentNode")
-            continue;
-            
-        val = node[keys[i]];
-        
-        // Skip non-objects
-        if (val !== Object(val)) 
-            continue;
-        
-        if (typeof val.type === "string") {
-        
-            // Nodes have a "type" property
-            fn(val);
-        
-        } else {
-        
-            // Iterate arrays
-            for (j = 0; j < (val.length >>> 0); ++j)
-                if (val[j] && typeof val[j].type === "string")
-                    fn(val[j]);
-        }
-    }
-}
-
