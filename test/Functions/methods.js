8436f41e13abe46d079b75f20f017d71543010ae
khs4473 <zenparsing@gmail.com>
2013-12-28 23:12:17 -0500

    Add a test.

--- test/Functions/methods.js
+++ test/Functions/methods.js
@@ -1,5 +1,39 @@
 [
 
+/** ({ x() {} }); **/
+{   type: "Script",
+    statements: [
+    
+    {   type: "ExpressionStatement",
+        expression:
+        
+        {   type: "ParenExpression",
+            expression:
+        
+            {   type: "ObjectLiteral",
+                properties: [
+                
+                {   type: "MethodDefinition",
+                    
+                    kind: "",
+                    
+                    name: 
+                    {   type: "Identifier",
+                        value: "x"
+                    },
+                    
+                    params: [],
+                        
+                    body: 
+                    {   type: "FunctionBody",
+                        statements: []
+                    }
+                }]
+            }
+        }
+    }]
+},
+
 /** ({ *x() {} }); **/
 {   type: "Script",
     statements: [
