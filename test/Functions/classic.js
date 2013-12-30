2d1bdaf7ee5df0a12f44b46c94b9f3d46ded811f
khs4473 <zenparsing@gmail.com>
2013-12-30 00:04:26 -0500

    Add test.

--- test/Functions/classic.js
+++ test/Functions/classic.js
@@ -1,5 +1,24 @@
 {
 
+/** function x() {} **/
+"function declaration": {
+    type: "Script",
+    statements: [
+    {   type: "FunctionDeclaration",
+        kind: "",
+        identifier:
+        {   type: "Identifier",
+            value: "x",
+            context: "declaration"
+        },
+        params: [],
+        body: 
+        {   type: "FunctionBody",
+            statements: []
+        }
+    }]
+},
+
 /** function yield() { "use strict" } **/
 "function identifier must follow strictness of body": {}
 
